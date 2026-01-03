aa0435d3ca790c54df6958d5c8a189c9ae20f1f0
MasleK <martin.dehnert@netzzwerge.de>
2026-01-03 22:01:46 +0100

    opengl canvas

--- AvaloniaWasmTest/OpenGlCanvas.cs
+++ AvaloniaWasmTest/OpenGlCanvas.cs
@@ -1,5 +1,12 @@
+namespace AvaloniaWasmTest;
+using Avalonia.OpenGL;
 using Avalonia.OpenGL.Controls;
 
 public class OpenGlCanvas : OpenGlControlBase
 {
+    protected override void OnOpenGlRender(GlInterface gl, int fb)
+    {
+        gl.ClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+        gl.Clear(GlConsts.GL_COLOR_BUFFER_BIT);
+    }
 }
