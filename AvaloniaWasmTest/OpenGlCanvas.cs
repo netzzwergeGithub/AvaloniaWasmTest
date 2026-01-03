7505206c32f7e8d088497a8ce47d3a54b95062f6
Solveit <nobody@fast.ai>
2026-01-03 21:20:04 +0000

    Request rendering

--- AvaloniaWasmTest/OpenGlCanvas.cs
+++ AvaloniaWasmTest/OpenGlCanvas.cs
@@ -4,6 +4,11 @@ using Avalonia.OpenGL.Controls;
 
 public class OpenGlCanvas : OpenGlControlBase
 {
+    public OpenGlCanvas()
+    {
+        RequestNextFrameRendering();
+    }
+
     protected override void OnOpenGlRender(GlInterface gl, int fb)
     {
         gl.ClearColor(0.2f, 0.3f, 0.8f, 1.0f);
