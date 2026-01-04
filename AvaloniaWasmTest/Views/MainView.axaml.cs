58e5506c827bc77342dc65cfcc6d2550cd48fe6e
MasleK <martin.dehnert@netzzwerge.de>
2026-01-04 21:52:16 +0100

    Canvas3D and platform separation added

--- AvaloniaWasmTest/Views/MainView.axaml.cs
+++ AvaloniaWasmTest/Views/MainView.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 
 namespace AvaloniaWasmTest.Views;
@@ -7,5 +8,15 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        if (OperatingSystem.IsWindows() || OperatingSystem.IsLinux())
+        {
+            Content = new Canvas3D()
+        }
+        else
+        {
+            var os = Environment.OSVersion;
+
+            throw new NotImplementedException("Operating System not sopported" + os.Platform);
+        }
     }
 }
