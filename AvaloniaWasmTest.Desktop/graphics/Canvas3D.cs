cda322d592bfe2446a7a480b21abfd521ace3af6
MasleK <martin.dehnert@netzzwerge.de>
2026-01-04 21:06:10 +0100

    Dektop renderer

---
+++ AvaloniaWasmTest.Desktop/graphics/Canvas3D.cs
@@ -0,0 +1,12 @@
+using Avalonia.OpenGL;
+using Avalonia.OpenGL.Controls;
+using AvaloniaWasmTest;
+
+public class Canvas3D : OpenGlControlBase
+{
+    I3DRenderer renderer;
+    protected override void OnOpenGlRender(GlInterface gl, int fb)
+    {
+        throw new System.NotImplementedException();
+    }
+}
